2e0e7d0479176725732fe261c242ef3e99eb8ae5
Yohei Nishimura <york.nishi@gmail.com>
2022-08-15 02:50:46 -0500

    delete unneeded files

--- messages.js
+++
@@ -1,5 +0,0 @@
-exports.messages = [
-	"You are a great.",
-	"You can accomplish anything.",
-	"Success is in your hands.",
-]
--- t1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-let messages = [
-  "tensai",
-  "arikitari",
-  "bonjin"
-  ];
-
-
